9090cafa6dc2b1762344450627b467993321c81f
Your Name <seytorin@yahoo.com>
2018-03-13 19:54:27 -0500

    Fixed login switch to register

--- Extro-master/extro/routes/auth.js
+++ Extro-master/extro/routes/auth.js
@@ -5,7 +5,7 @@ module.exports = function(app,passport){
 app.get('/signup', authController.signup);
 
 
-// app.get('/signin', authController.signin);
+app.get('/signin', authController.signin);
 
 
 app.post('/signup', passport.authenticate('local-signup',  { successRedirect: '/profile',
